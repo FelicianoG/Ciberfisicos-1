194b8460098b59b140d5bdbfd69b766441870ad2
FelicianoG <felicianogc@gmail.com>
2023-11-17 11:32:51 -0700

    Implemented interpolation

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -4,11 +4,48 @@
 import { handlePlantAnimation } from "../handlerFunctions";
 import { Slider } from "./Slider";
 import { ButtonGrid, ButtonStyles } from "../styles";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 // eslint-disable-next-line react/prop-types
 export const SideMenu = ({ orbitRef, rootBone }) => {
   const [extended, setExtended] = useState(false);
+  const position = useRef(0);
+  const animationFrame = useRef();
+
+  function animateNumericVariable(startValue, endValue, callback) {
+    const speed = 0.001;
+    const distance = endValue - startValue;
+    const duration = Math.abs(distance) / speed;
+
+    // eslint-disable-next-line no-undef
+    const startTime = performance.now();
+
+    function update() {
+      // eslint-disable-next-line no-undef
+      const currentTime = performance.now();
+      const elapsedTime = currentTime - startTime;
+      const progress = Math.min(elapsedTime / duration, 1);
+      const animatedValue = startValue + progress * distance;
+
+      // Update your variable or DOM element with the animated value
+      // For example, updating the left position of an element
+      handlePlantAnimation(animatedValue, rootBone);
+      position.current = animatedValue;
+
+      if (progress < 1) {
+        // eslint-disable-next-line no-undef
+        requestAnimationFrame(update);
+      } else {
+        if (callback) {
+          callback();
+        }
+      }
+    }
+
+    // eslint-disable-next-line no-undef
+    animationFrame.current = requestAnimationFrame(update);
+  }
+
   return (
     <div className={`side-menu${extended ? " extended" : ""}`}>
       <button
@@ -27,13 +64,21 @@ export const SideMenu = ({ orbitRef, rootBone }) => {
           min={0}
           step={0.01}
           onChange={(e) => {
-            handlePlantAnimation(e.target.value, rootBone);
+            if (animationFrame.current) {
+              // eslint-disable-next-line no-undef
+              cancelAnimationFrame(animationFrame.current);
+            }
+            // eslint-disable-next-line no-undef
+            console.log("position start " + position.current);
+            animateNumericVariable(position.current, e.target.value, () => {
+              // eslint-disable-next-line no-undef
+              console.log("position end " + position.current);
+            });
           }}
         />
-        <input
-          onChange={(e) => handlePlantAnimation(e.target.value, rootBone)}
-        ></input>
       </section>
+
+      <h3>Camera Control</h3>
       <ButtonGrid>
         <CamButton orbitRef={orbitRef} title={"A1"} />
         <CamButton orbitRef={orbitRef} title={"A2"} />
